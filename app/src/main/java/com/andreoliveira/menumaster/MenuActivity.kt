6efb7325fa55880b88a27a10b8a9ca180983e84f
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2026-04-11 20:13:23 -0300

    Criação das telas de boas vindas e cardápios.

--- app/src/main/java/com/andreoliveira/menumaster/MenuActivity.kt
+++ app/src/main/java/com/andreoliveira/menumaster/MenuActivity.kt
@@ -1,38 +1,27 @@
 package com.andreoliveira.menumaster
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.andreoliveira.menumaster.databinding.ActivityMainBinding
+import com.andreoliveira.menumaster.databinding.ActivityMenuBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MenuActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var  binding: ActivityMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.btnDiscover.setOnClickListener(this)
-    }
-
-    override fun onClick(view: View?) {
-        if (view?.id == R.id.btnDiscover) {
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
         }
     }
 }
