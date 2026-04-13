6ce25a696eb8a2b28c1c4c7eab0077d0c8688359
PC_do_Batista <gabriel.code22@gmail.com>
2026-04-12 22:32:42 -0300

    Adicionado botão checkbox de favoritar na pagina de detalhes

--- app/src/main/java/com/andreoliveira/menumaster/DetalhePratoActivity.kt
+++ app/src/main/java/com/andreoliveira/menumaster/DetalhePratoActivity.kt
@@ -5,6 +5,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+import android.widget.CheckBox
+import android.widget.Toast
+
 class DetalhePratoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,5 +38,22 @@ class DetalhePratoActivity : AppCompatActivity() {
                 "👤 Serve 1 pessoa"
             else
                 "👥 Serve $pessoas pessoas"
+
+        val prefs = getSharedPreferences("favoritos", MODE_PRIVATE)
+        val check = findViewById<CheckBox>(R.id.checkFavoritoDetalhe)
+
+        check.isChecked = prefs.getBoolean(nome, false)
+
+        check.setOnCheckedChangeListener { _, isChecked ->
+
+            prefs.edit().putBoolean(nome, isChecked).apply()
+
+            if (isChecked) {
+                Toast.makeText(this, "$nome adicionado aos favoritos", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "$nome removido dos favoritos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
+
